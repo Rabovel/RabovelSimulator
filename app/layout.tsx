2f46187b1ef0db4451293c28c933a4707d7070ab
Code~warrior200 <61137642+code-warrior200@users.noreply.github.com>
2025-10-07 20:34:18 +0100

    git commit -m

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Trading Dashboard",
+  title: "Rabovel Simulator Dashboard",
   description: "Manage your portfolio, funds, and transactions with ease",
 };
 
